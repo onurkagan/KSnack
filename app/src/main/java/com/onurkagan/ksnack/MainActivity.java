89df19978f1269087fccd6b95d7ff0e07fc54f43
onurkagan <onurkaganaldemir@gmail.com>
2018-10-29 13:31:07 +0300

    Minimal snackbar.

--- app/src/main/java/com/onurkagan/ksnack/MainActivity.java
+++ app/src/main/java/com/onurkagan/ksnack/MainActivity.java
@@ -2,6 +2,9 @@ package com.onurkagan.ksnack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.onurkagan.ksnack_lib.KSnack;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final KSnack kSnack = new KSnack(MainActivity.this);
+
+        findViewById(R.id.activity_main_btn_show).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kSnack.show();
+            }
+        });
+
+        findViewById(R.id.activity_main_btn_dissmis).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kSnack.dismiss();
+            }
+        });
     }
 }
